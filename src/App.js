730fe739cb460b79b5525bf633d31a9328028ef2
mwaaark <kramtrebor1995@gmail.com>
2023-04-17 15:45:13 +0800

    using transition events

--- src/App.js
+++ src/App.js
@@ -36,6 +36,29 @@ class App extends Component {
         </button>
 
         <br />
+        <Transition
+          in={this.state.showBlock}
+          timeout={1000}
+          mountOnEnter
+          unmountOnExit
+          onEnter={() => console.log("onEnter")}
+          onEntering={() => console.log("onEntering")}
+          onEntered={() => console.log("onEntered")}
+          onExit={() => console.log("onExit")}
+          onExiting={() => console.log("onExiting")}
+          onExited={() => console.log("onExited")}
+        >
+          {(state) => (
+            <div
+              style={{
+                backgroundColor: "red",
+                width: 100,
+                height: 100,
+                margin: "auto",
+              }}
+            ></div>
+          )}
+        </Transition>
 
         <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
 
