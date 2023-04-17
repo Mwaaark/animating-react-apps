5b26e04560a3bd22430bf2a9079f8ecbd8adbced
mwaaark <kramtrebor1995@gmail.com>
2023-04-17 15:39:10 +0800

    adding animation timing

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Transition } from "react-transition-group";
 
 import "./Modal.css";
 
-import { Transition } from "react-transition-group";
+const animationTiming = {
+  enter: 400,
+  exit: 1000,
+};
 
 const modal = (props) => {
   return (
-    <Transition mountOnEnter unmountOnExit in={props.show} timeout={300}>
+    <Transition
+      mountOnEnter
+      unmountOnExit
+      in={props.show}
+      timeout={animationTiming}
+    >
       {(state) => {
         const cssClasses = [
           "Modal",
